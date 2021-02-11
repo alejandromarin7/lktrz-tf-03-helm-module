af352d44b7c9f36dca6e613d678286bc8b9bf309
alejandromarin7 <alejandro.padilla@digitalonus.com>
2021-02-11 13:21:10 -0600

    (modify)add values to helm

--- variables.tf
+++ variables.tf
@@ -1,15 +1,9 @@
-variable set {
-  type = list(object({
-    namewp      = name
-    namechartwp = name_chart
-    namehelpwp  = name_helm
-  }))
+variable "set" {
+  default = [
+    {
+      namewp      = "name"
+      namechartwp = "wordpress"
+      namehelpwp  = "helmwphart"
+    }
+  ]
 }
-#variable set {
-#  type       = list(map(string))
-#  default    = null
-#}
-variable name {}
-variable name_chart {}
-variable name_helm {}
-
