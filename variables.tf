8dfc9ff02d340875daeb1d740d0ecec33da08372
alejandromarin7 <alejandro.padilla@digitalonus.com>
2021-02-11 13:13:59 -0600

    (modify)add values to helm

--- variables.tf
+++ variables.tf
@@ -1,7 +1,22 @@
 variable set {
-  type       = list(map(string))
-  default    = null
+  type = list(object({
+    namewp      = name
+    namechartwp = name_chart
+    namehelpwp  = name_helm
+  }))
+  default = [
+    {
+      namewp = wpdefault
+      namechartwp = wordpress
+      namehelpwp  = helmwphart
+    }
+  ]
 }
+#variable set {
+#  type       = list(map(string))
+#  default    = null
+#}
 variable name {}
 variable name_chart {}
 variable name_helm {}
+
