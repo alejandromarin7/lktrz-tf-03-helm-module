0a39808ab7b20db89838616029373e5dce300f31
alejandromarin7 <alejandro.padilla@digitalonus.com>
2021-02-11 13:37:36 -0600

    (modify)add values to helm

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "helm_release" "wordpress" {
   dynamic "set" {
     for_each = [for s in set: {
       name   = s.name
-      chart  = s.chart)
+      chart  = s.chart
     }]
     content {
       name_helm  = set.value.name
